4969f3778186f40bc985e2378e7e2effcb3be5bc
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-08-24 16:25:25 +0400

    LaunchSettings Changed

--- EfCoreInAction.Api/Startup.cs
+++ EfCoreInAction.Api/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EfCoreInAction.Core;
+using EfCoreInAction.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +27,7 @@ namespace EfCoreInAction.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IRepository, Repository>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
