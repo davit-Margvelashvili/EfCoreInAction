1b74223e88a02f1b18547e719c20cfc145a28093
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-08-24 16:01:39 +0400

    Migrations Added

---
+++ EfCoreInAction.Data/LibraryContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EfCoreInAction.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreInAction.Data
+{
+    public class LibraryContext : DbContext
+    {
+        private const string ConnectionString =
+            @"Server = (localdb)\MSSQLLocalDb; Database = LibraryDb; Trusted_Connection = true";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ EfCoreInAction.Data/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EfCoreInAction.Core;
+using EfCoreInAction.Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreInAction.Data
+{
+    public class Repository : IRepository
+    {
+        public List<Book> ListAll()
+        {
+            using (var db = new LibraryContext())
+            {
+                return db.Books.AsNoTracking().Include(b => b.Author).ToList();
+            }
+        }
+    }
+}
