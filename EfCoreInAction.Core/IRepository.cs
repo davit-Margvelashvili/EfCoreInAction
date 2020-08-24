d3898bdd8b753cdb4fc5c8a84b80dc1530f55965
Davit Margvelashvili <d.margvelashvili@altasoft.ge>
2020-08-24 16:01:57 +0400

    Models Added

---
+++ EfCoreInAction.Core/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EfCoreInAction.Core.Models;
+
+namespace EfCoreInAction.Core
+{
+    public interface IRepository
+    {
+        List<Book> ListAll();
+    }
+}
---
+++ EfCoreInAction.Core/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace EfCoreInAction.Core.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string WebUrl { get; set; }
+    }
+}
